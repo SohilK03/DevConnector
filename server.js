33bc5ed350a433463d154fd45160c6ffdf554e11
Sohil Khattar <sohilkhattar123@gmail.com>
2020-07-03 13:03:10 +0530

    connected backend and frontend

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/', (req, res) => {
 app.use('/api/users', users);
 app.use('/api/profiles', profiles);
 app.use('/api/posts', posts);
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
 	console.log(`The server running on port ${PORT}`);
