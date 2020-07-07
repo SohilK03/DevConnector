fed12a2e1db377d5d08746e6aa5ec7a00d695bbf
Sohil Khattar <sohilkhattar123@gmail.com>
2020-07-07 13:47:21 +0530

    Added redux store

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 // eslint-disable-next-line
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
@@ -9,22 +11,26 @@ import Register from './Auth/Register';
 
 import './App.css';
 
-function App() {
-	return (
-		<Router>
-			<div className='App'>
-				<Navbar />
-				<Route exact path='/' component={Landing} />
-				<div className='container'>
-					<Route exact path='/register' component={Register} />
-				</div>
-				<div className='container'>
-					<Route exact path='/login' component={Login} />
-				</div>
-				<Footer />
-			</div>
-		</Router>
-	);
+class App extends Component {
+	render() {
+		return (
+			<Provider store={store}>
+				<Router>
+					<div className='App'>
+						<Navbar />
+						<Route exact path='/' component={Landing} />
+						<div className='container'>
+							<Route exact path='/register' component={Register} />
+						</div>
+						<div className='container'>
+							<Route exact path='/login' component={Login} />
+						</div>
+						<Footer />
+					</div>
+				</Router>
+			</Provider>
+		);
+	}
 }
 
 export default App;
