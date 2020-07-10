34592fe56bdd745f39582ef0f6fb7e7fcde3b2be
Sohil Khattar <sohilkhattar123@gmail.com>
2020-07-10 14:19:02 +0530

    Error Correction

--- client/src/components/Dashboard/ProfileActions.js
+++ client/src/components/Dashboard/ProfileActions.js
@@ -7,15 +7,15 @@ function ProfileActions() {
 			<div className='btn-group mb-4' role='group'>
 				<Link to='/edit-profile' className='btn btn-light'>
 					<i className='fas fa-user-circle text-info mr-1'></i> Edit Profile
-				</a>
+				</Link>
 				<Link to='/add-experience' className='btn btn-light'>
 					<i className='fab fa-black-tie text-info mr-1'></i>
 					Add Experience
-				</a>
+				</Link>
 				<Link to='/add-education' className='btn btn-light'>
 					<i className='fas fa-graduation-cap text-info mr-1'></i>
 					Add Education
-				</a>
+				</Link>
 			</div>
 		</div>
 	);
