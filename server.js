1acb0db5dcaa00ea80ac49650fd51bf26595ed48
Sohil Khattar <sohilkhattar123@gmail.com>
2020-07-14 18:42:49 +0530

    Prepared for deploy

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const posts = require('./routes/api/posts');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const app = express();
+const path = require('path');
 
 // Body Parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -34,6 +35,15 @@ app.get('/', (req, res) => {
 app.use('/api/users', users);
 app.use('/api/profiles', profiles);
 app.use('/api/posts', posts);
+
+// Serve Static assets if in porduction
+if (process.env.NODE_ENV === 'production') {
+	// Set the static folder
+	app.use(express.static('client/buid'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
